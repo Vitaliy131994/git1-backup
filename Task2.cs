2771e5e95af2668e80fd979f2308513ac9241ab7
Vitaliy S <BYKET_1994@UKR.NET>
2019-08-21 13:15:03 +0300

    gitignore

--- Task2.cs
+++ Task2.cs
@@ -1,7 +1,8 @@
-﻿namespace Task2
+﻿namespace Program
 {
+
     class Program
-        {
+    {
         static void Main(string[] args)
         {
             Console.WriteLine("Enter x1");
@@ -15,8 +16,8 @@
             Point Point1 = new Point(x1, y1);
             Point Point2 = new Point(x2, y2);
             Rectangle R = new Rectangle(Point1, Point2);
-            Console.WriteLine("Perum:"+R.Perum());
-            Console.WriteLine("Square:"+R.Square());
+            Console.WriteLine("Perum:" + R.Perum());
+            Console.WriteLine("Square:" + R.Square());
             Console.ReadKey();
 
         }
@@ -24,27 +25,30 @@
     class Rectangle
     {
         Point a;
-        public int ab {
+        public int ab
+        {
             get { return c.x - a.x; }
-        
+
         }
         Point c;
         public int bc
         {
             get { return c.y - a.y; }
         }
-        public Rectangle(Point a1, Point c1) { a = a1;c = c1;  }
-        public int Perum() {
-            
-            return (ab + bc) * 2; 
-                }
-        public int Square() {
-              return (ab * bc);
+        public Rectangle(Point a1, Point c1) { a = a1; c = c1; }
+        public int Perum()
+        {
+
+            return (ab + bc) * 2;
         }
+        public int Square()
+        {
+            return (ab * bc);
         }
+    }
     class Point
     {
-        public Point(int x1, int y1) { x = x1;y = y1; } 
+        public Point(int x1, int y1) { x = x1; y = y1; }
         public int x;
         public int y;
     }
